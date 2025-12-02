3186643076d581a5e120f345c991a3872ea9f0df
Segaara <adesegoro@gmail.com>
2025-12-02 20:18:30 +0700

    save change app.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SideBar from "./src/navigator/side-bar";
 export default function App() {
   return (
